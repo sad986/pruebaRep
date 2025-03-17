885e915a78d1d45afe4476be2422f83bdca7d1a7
Adrian Mariscal <adrianmariscalguerrero1@gmail.com>
2025-03-17 23:26:28 +0100

    Add new method

--- src/main/java/org/iis2024/Date.java
+++ src/main/java/org/iis2024/Date.java
@@ -20,6 +20,10 @@ public class Date {
   private final int month;
   private final int year;
 
+  public void holaMundo(){
+    System.out.println("Hola Mundo");
+  }
+
   /**
    * Retrieves the day component of this date.
    *
